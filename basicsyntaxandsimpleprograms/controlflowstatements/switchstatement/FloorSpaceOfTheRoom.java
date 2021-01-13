08498ba1a6ae2d1a84fb68342c2ca91e77d50735
Ruari Mears <earlution@gmail.com>
2021-01-13 18:32:54 +0000

    Problem: Floor-space of the room program stub

---
+++ basicsyntaxandsimpleprograms/controlflowstatements/switchstatement/FloorSpaceOfTheRoom.java
@@ -0,0 +1,72 @@
+package basicsyntaxandsimpleprograms.controlflowstatements.switchstatement;
+
+
+/**
+ * Residents of the country of Malevia often experiment with the plan of their rooms. Rooms 
+ * can be triangular, rectangular, and round.
+ * 
+ * Write a program that calculates the floorage of the rooms.
+ * 
+ * Input data format
+ * The type of the room shape and the relevant parameters.
+ * 
+ * Output data format
+ * The area of the resulting room.
+ * 
+ * Note: that the value of 3.14 is used instead of the number π in Malevia.
+ * Hint: Math library may help you to solve this task.
+ * 
+ * Input format used by the Malevians:
+ *
+ * triangle
+ * a
+ * b
+ * c
+ * 
+ * where a, b and c — lengths of the triangle sides.
+ * 
+ * rectangle
+ * a
+ * b
+ * 
+ * where a and b — lengths of the rectangle sides.
+ * 
+ * circle
+ * r
+ * 
+ * where r — circle radius.
+ * 
+ * Note, the input values (a, b, c, r) are doubles and your answer should be, too.
+ * 
+ * Sample Input 1:
+ * rectangle
+ * 4
+ * 10
+ * 
+ * Sample Output 1:
+ * 40.0
+ * 
+ * Sample Input 2:
+ * circle
+ * 5
+ * 
+ * Sample Output 2:
+ * 78.5
+ * 
+ * Sample Input 3:
+ * triangle
+ * 3
+ * 4
+ * 5
+ * 
+ * Sample Output 3:
+ * 6.0
+ *
+ * @author (Ruari Mears)
+ * @version (13.01.2021)
+ */
+public class FloorSpaceOfTheRoom {
+    public static void main(String[] args) {
+        // put your code here
+    }
+}
